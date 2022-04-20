3c77656db557de4ac83f31df5c3c007a2c86d8f4
Michael Kendrick <michael@locumsmart.net>
2022-04-20 17:34:26 -0500

    added animals

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { animals } = require("./data/animals");
+const fs = require("fs");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -42,6 +47,17 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -52,9 +68,40 @@ app.get("/api/animals", (req, res) => {
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
-  res.json(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.post("/api/animals", (req, res) => {
+  req.body.id = animals.length.toString();
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+  }
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
 });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
