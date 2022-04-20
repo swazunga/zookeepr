62ad59e156fdb6eeeaa86f40ffa0762a3041d3ce
Michael Kendrick <michael@locumsmart.net>
2022-04-20 18:21:21 -0500

    wired up

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -86,8 +87,20 @@ app.post("/api/animals", (req, res) => {
   res.json(animal);
 });
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 function validateAnimal(animal) {
@@ -105,3 +118,7 @@ function validateAnimal(animal) {
   }
   return true;
 }
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
